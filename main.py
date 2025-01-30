2d892d995f4eb974109dedd788971fa90a3dc4e8
csizmadia32 <csizmadia32@gmail.com>
2025-01-30 21:46:40 +0100

    update

--- main.py
+++ main.py
@@ -2,31 +2,52 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-#    print(f"{num_words} words found in the document")
-#    print(count_character(text))
-    char_frequency_dict = count_character(text)
-    sorted_counts = sorted(char_frequency_dict.items(), key=lambda item: item[1], reverse=True)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-    for char, count in sorted_counts:
-        if char.isalpha():
-            print(f"The '{char}' character was found {count} times")
-    
-def count_character(text):
-    char_counts = {}
-    for char in text:
-        char_lower = char.lower()
-        if char_lower in char_counts:
-            char_counts[char_lower] += 1
-        else:
-            char_counts[char_lower] = 1
-    return char_counts
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
 
 def get_num_words(text):
     words = text.split()
     return len(words)
 
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
 
 main()
